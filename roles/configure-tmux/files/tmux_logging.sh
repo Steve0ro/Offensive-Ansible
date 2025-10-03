7ac939fd8e5bea5144653f1db31efba069b6815f
Steve0ro <68754691+Steve0ro@users.noreply.github.com>
2025-10-02 21:54:08 -0400

    Added tmux logger

--- roles/configure-tmux/files/tmux_logging.sh
+++ roles/configure-tmux/files/tmux_logging.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # Original Author: Brian - https://www.hackernotebook.com/posts/automatic-tmux-logging/
 
-tmux pipe-pane -o 'exec bash -c "while IFS= read -r line; do printf \"%%(%%Y%%m%%dT%%H%%M%%S%%z)T: %%s\n\" -1 \"\$line\"; done"\; exec cat >> $HOME/Logs/#S-#W-#I-#P.log' \; display-message 'Started logging to #S-#W-#I-#P.log'
+tmux pipe-pane -o 'stdbuf -oL sed -r "s/\x1B\[[0-9;]*[mK]//g" | stdbuf -oL ts "%Y-%m-%dT%H:%M:%S%z: " >> $HOME/Logs/#S-#W-#I-#P.log' \; display-message 'Started logging to #S-#W-#I-#P.log'
